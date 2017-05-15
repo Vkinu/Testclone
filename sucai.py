16eb58843d067db3a5adb74b82cf02a72cdb5224
Vkinu <1327492577@qq.com>
2017-05-15 14:39:42 +0800

    Update sucai.py

--- sucai.py
+++ sucai.py
@@ -18,25 +18,13 @@ for i in range(1000):
         result = html.xpath('//p/a/img/@src')
         for i in result:
             print i
-            # url = "https://m.gewara.com/captcha.xhtml?captchaId=Yxbnb2FHSVaya39747fa9d94&r=1491978461785"
-            # request2 = urllib2.Request(i, headers=header)
-            # res2= urllib2.urlopen(request).read()
-            # print res2
             s = j
             urllib.urlretrieve(i, 'c:\\IMG\\%s.jpg' % s)
         j = j + 1
     except :
         j=j+1
         print u"不行加1"
-        if
-
-
-
-
-
-
-    # image = Image.open(cStringIO.StringIO(res2))
-    # image.save('C://AAAAAA.jpg')
+        
 
 
 
