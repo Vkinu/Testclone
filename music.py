04eef06d87ff2e3fa019c573dcbbf7d5bc511067
myvkin <1327492577@qq.com>
2017-05-16 18:10:50 +0800

    music

--- music.py
+++ music.py
@@ -1,68 +1,131 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# coding=utf-8
 import urllib
 import re
 import time
 import cookielib
 import urllib2
 import wget
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.action_chains import ActionChains
-# driver = webdriver.Chrome()
-imageUrl="http://download.file.xiami.com/download/get?id=1008716&type=song&quality=1&crc=c0182098ac78cee07a84bcf813649e8e"
-
-loginUrl = 'http://www.xiami.com/song/xNbdb0dbb30?spm=a1z1s.3521865.23309997.1.Ebyoym'  # 登录教务系统的URL，成绩查询网址
+import requests
+from lxml import etree
 # #创建MozillaCookieJar实例对象
 cookie = cookielib.MozillaCookieJar()
 #从文件中读取cookie内容到变量
 cookie.load('cookie3.txt', ignore_discard=True, ignore_expires=True)
+r = str(int(round(time.time() * 1000)))
 #创建请求的request
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-    "Referer" : "http://www.xiami.com/song/xNbdb0dbb30?spm=a1z1s.3521865.23309997.1.Ebyoym",
+    "Referer" : "http://www.xiami.com/song/bpVOa3dc7?spm=a1z1s.6626001.229054121.2.YUCnCX",
+    "X-Requested-With" : "XMLHttpRequest",
+    "Host" : "www.xiami.com",
+    "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
 }
-req2 = urllib2.Request("http://www.xiami.com/download/get-link?_=1494835079916&type=song&id=1008716&quality=1",headers=header)
+proxy = {'http':'180.97.235.30:80'}
+proxy_support = urllib2.ProxyHandler(proxy)
+q=1
+id1 = 1795888147
+id = str(id1)
+i=0
 
-req = urllib2.Request("http://www.xiami.com/download/get-link?ping=1&_=1494835081916&type=song&id=1008716&quality=1",headers=header)
 
-req3 =urllib2.Request("http://download.file.xiami.com/download/get?id=1008716&type=song&quality=1&crc=c0182098ac78cee07a84bcf813649e8e")
-#利用urllib2的build_opener方法创建一个opener
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
-response = opener.open(req)
+req = urllib2.Request("http://www.xiami.com/download/get-link?_=" + r + "&type=song&id=" + id + "&quality=1",
+                      headers=header)
+
+req2 = urllib2.Request(
+    "http://www.xiami.com/download/get-link?ping=1&_=" + r + "&type=song&id=" + id + "&quality=1",
+    headers=header)
+time.sleep(3)
+# 利用urllib2的build_opener方法创建一个opener
+opener = urllib2.build_opener(proxy_support,urllib2.HTTPCookieProcessor(cookie))
+
+
+reqid = urllib2.Request("http://www.xiami.com/song/xNdulbc1e05?spm=a1z1s.6659513.0.0.uZQJJd",
+                      headers=header)
+responseid = opener.open(reqid).read()
+
+html = etree.HTML(responseid)
+resulttitle = html.xpath('//html/head/meta[10]/@content')
+title = resulttitle[0].encode("utf8")
+print title
+resulturl = html.xpath('//html/head/meta[12]/@content')
+id2 = resulturl[0][48:]
+# print id
+#
+# id = str(id2)
+
+
+
 response2 = opener.open(req2)
-print response.read()
-print response2.read()
-response3 = opener.open(req3)
-# print response.read()
-# print response2.read()
-# # print response3.read()
-#
-#
-#
-# driver.get(imageUrl)
-urllib.urlretrieve('https://baike.baidu.com/pic/Python/407313/0/0eb30f2442a7d9331abfc6f3ad4bd11373f0011e?fr=lemma&ct=single#aid=0&pic=0eb30f2442a7d9331abfc6f3ad4bd11373f0011e', 'C:/BingYu.jpg')
-# urllib.urlretrieve("http://download.file.xiami.com/download/get?id=1008716&type=song&quality=1&crc=c0182098ac78cee07a84bcf813649e8e", u"平哥aaaa.mp3")
-# # baseurl = "http://music.baidu.com"
-# # url = "http://music.baidu.com/search/tag?key=经典流行"
-# # html = urllib.urlopen(url).read()
-# # uri = re.findall(r'/song/\d+', html, re.M)
-# # lst = []
-# # print lst
-# # for i in uri:
-# #     link = baseurl+i+"/download"
-# #     lst.insert(0, link)
-# # for k in lst:
-# #     res = urllib.urlopen(k).read()
-# #     down = re.search('http://[^ ]*xcode.[a-z0-9]*' , res, re.M).group()
-# #     s1 = re.search('title=".*',res, re.M).group()
-# #     s2 = re.search('>.*<.a', s1, re.M).group()
-# #     s3 = s2[1:-3]
-# #     print s3
-# #     down="http://music.baidu.com/song/100575177"
-# #     urllib.urlretrieve(down, s3+".mp3")
-#
-# down="http://music.baidu.com/song/100575177"
-# s3=int(time.time())
-# s4=str(s3)
-# urllib.urlretrieve(down, u"我很棒"+s4+".mp3")
+response = opener.open(req)
+result = response2.read()
+result2 = result.replace("\\", '')
+print result2
+pattern2 = re.compile(r'http:.*"')
+try:
+    mattch2 = re.search(pattern2, result2).group()[:-1]
+    print mattch2
+    response3 = opener.open(mattch2)
+    stream = response3.read()
+    f = open('c:\\IMG\\' + title + '.mp3', 'wb')
+    f.write(stream)
+    f.close()
+    s = 1
+    print "完成啦"
+except:
+    i = i + 0
+    s = 0
+while s == 0:
+    time.sleep(1)
+    req = urllib2.Request("http://www.xiami.com/download/get-link?_=" + r + "&type=song&id=" + id + "&quality=1",
+                          headers=header)
+
+    req2 = urllib2.Request(
+        "http://www.xiami.com/download/get-link?ping=1&_=" + r + "&type=song&id=" + id + "&quality=1",
+        headers=header)
+    time.sleep(3)
+    # 利用urllib2的build_opener方法创建一个opener
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+    response2 = opener.open(req2)
+    response = opener.open(req)
+    result = response2.read()
+    result2 = result.replace("\\", '')
+    print result2
+    pattern2 = re.compile(r'http:.*"')
+    try:
+        mattch2 = re.search(pattern2, result2).group()[:-1]
+        print mattch2
+        response3 = opener.open(mattch2)
+        stream = response3.read()
+        f = open('c:\\IMG\\' + title + '.mp3', 'wb')
+        f.write(stream)
+        f.close()
+        s = 1
+    except:
+        i = i + 0
+        s = 0
+        if s == 1:
+            break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
